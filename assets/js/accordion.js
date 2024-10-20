e6370c6eee55c9ec3e4ae60d2b9ffdfe74ac86c6
muiruri3000 <josee.muiruri95@gmail.com>
2024-10-20 17:00:00 +0300

    update accordion to display first item by default

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,6 +1,13 @@
 let accordionItems = document.querySelectorAll('.accordion-item');
 
+if(accordionItems.length > 0){
+    let firstItem = accordionItems[0];
 
+    let firstPanel = firstItem.parentElement.nextElementSibling;
+
+    firstPanel.classList.add('active');
+    firstPanel.style.maxHeight = firstPanel.scrollHeight + 'px';
+}
 
 accordionItems.forEach((item)=>{
     item.addEventListener('click',()=>{
